f4672986956ceb6c0693a34c3fa50b0a247a5c6f
Mohammed Abdulrazeg <github@mzawi.me>
2023-08-08 11:16:56 +0100

    Add Serilog

--- Library.Api/Program.cs
+++ Library.Api/Program.cs
@@ -1,9 +1,17 @@
 using FluentValidation;
 using Library.Api.Data;
 using Library.Api.Endpoints.Internal;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// remove default logging providers
+builder.Logging.ClearProviders();
+// Serilog configuration        
+var logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+// Register Serilog
+builder.Logging.AddSerilog(logger);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
